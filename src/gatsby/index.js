bcdd8b6a05b135f4eccba22239f579b3f82b5eaa
dmitry <dmitry@scaleflex.cloud>
2020-04-16 18:05:19 +0200

    chore: add version without polyfills for gatsby

---
+++ src/gatsby/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ImgComponent from '../img';
+import BackgroundImgComponent from '../background';
+import CloudimageProvider, { CloudimageContext } from '../provider';
+
+
+const Img = (props = {}) => {
+  return (
+    <CloudimageContext.Consumer>
+      {(context = {}) => <ImgComponent {...props} config={context.config}/>}
+    </CloudimageContext.Consumer>
+  )
+}
+
+const BackgroundImg = (props = {}) => {
+  return (
+    <CloudimageContext.Consumer>
+      {(context = {}) => <BackgroundImgComponent {...props} config={context.config }/>}
+    </CloudimageContext.Consumer>
+  )
+}
+
+export default Img;
+
+export { CloudimageProvider, Img, BackgroundImg };
