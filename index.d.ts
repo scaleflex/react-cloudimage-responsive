32c5f4880f05ee29b6b5ec86b5d16286c67e7a85
François Voron <fvoron@gmail.com>
2020-06-30 12:03:19 +0200

    Add TypeScript declaration

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+declare module 'react-cloudimage-responsive' {
+
+  type Params = string | Record<string, string>;
+
+  export interface CloudimageProviderConfig {
+    token: string;
+    baseURL?: string;
+    doNotReplaceURL?: boolean;
+    lazyLoading?: boolean;
+    lazyLoadOffset?: number;
+    params?: Params;
+    placeholderBackground?: string;
+    lowQualityPreview?: {
+      minImgWidth: number;
+    }
+    presets?: Record<string, string>;
+    limitFactor?: number;
+    devicePixelRatioList?: number[];
+    delay?: number;
+  }
+
+  class CloudimageProvider extends React.Component<{ config: CloudimageProviderConfig }> { }
+
+  interface ImgProps {
+    src: string;
+    width?: string;
+    height?: string;
+    params?: Params;
+    sizes?: Record<string, any>;
+    ratio?: number;
+    lazyLoading?: boolean;
+    lazyLoadConfig?: Record<string, any>;
+  }
+
+  class BackgroundImg extends React.Component<ImgProps & React.ComponentProps<'img'>> { }
+
+  export default class Img extends React.Component<ImgProps & React.ComponentProps<'img'>> { }
+  
+}
