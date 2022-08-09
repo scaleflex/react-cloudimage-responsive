99f6e555467c2d46f4d666851c4de35a01bce531
Amr Wagdy <wagdy@scaleflex.com>
2022-08-09 16:51:58 +0200

    improvement: Update CDN links

--- examples/src/index.js
+++ examples/src/index.js
@@ -6,11 +6,11 @@ import App from './components/app';
 
 const cloudimageConfig = {
   token: 'demo',
-  baseURL: 'https://cloudimage.public.airstore.io/demo/',
+  baseURL: 'https://scaleflex.cloudimg.io/v7/demo/',
+  doNotReplaceURL: true,
   params: 'ci_info=1&org_if_sml=1',
   placeholderBackground: '#e1e1e1',
   limitFactor: 10,
-  apiVersion: 'v7',
   lowQualityPreview: {
     minImgWidth: 150,
   },
